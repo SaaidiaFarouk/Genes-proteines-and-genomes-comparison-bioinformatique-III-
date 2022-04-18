19e9a89b7d6115ec04b50497bf769fbdc3b3d505
SAAIDIA Farouk <farouk.saadia@insat.u-carthage.tn>
2022-04-18 23:51:22 +0000

    Multiple Sequence Alignement

--- Week 3/Multiple_sequence_alignement.py
+++ Week 3/Multiple_sequence_alignement.py
@@ -1,21 +1,153 @@
-def scoringdnas(dnas):
-    v=dnas[0]
-    w=dnas[1]
-    u=dnas[2]
-    scorex=list()
-    for i in v : 
-        scorex.append([])
-        for j in w : 
-            scorex[i].append([])  
-            for k in u : 
-                scorex[i][j].append(0)
+def MultipleLongestCommonSubsequence_simplest(seq1, seq2, seq3):
+    len1 = len(seq1)
+    len2 = len(seq2)
+    len3 = len(seq3)
+    
+    ### part 1: create backtrack matrix
+    
+    # backtrack matrix with blank initial values
+    bt = [[['_'] * len3 for j in range(len2)]
+             for i in range(len1)
+         ]
+    
+    # score matrix with blank initial values
+    s = [[[0] * (len3 + 1) for j in range(len2 + 1)]
+            for i in range(len1 + 1)
+        ]
+    
+    # fill in matrices cell by cell
+    for i, ch1 in enumerate(seq1, start=1):
+        for j, ch2 in enumerate(seq2, start=1):
+            for k, ch3 in enumerate(seq3, start=1):
+                match = 1 if (ch1 == ch2 == ch3) else 0
+                cs = max(s[i][j-1][k-1],           # cs -current value in score matrix
+                         s[i-1][j][k-1],           #    =s[i][j][k]
+                         s[i-1][j-1][k],
+                         s[i][j][k-1],
+                         s[i][j-1][k],
+                         s[i-1][j][k],
+                         s[i-1][j-1][k-1] + match)
+                
+                if cs == s[i-1][j-1][k-1] + match:
+                    cbt = 'd'                      # cbt -current value in backtrack matrix
+                elif cs == s[i][j-1][k-1]:         #     =bt[i-1][j-1][k-1]
+                    cbt = '⇒'
+                elif cs == s[i-1][j][k-1]:
+                    cbt = '⇓'
+                elif cs == s[i-1][j-1][k]:
+                    cbt = '↘'
+                elif cs == s[i][j][k-1]:
+                    cbt = '⨂'
+                elif cs == s[i][j-1][k]:
+                    cbt = '→'
+                elif cs == s[i-1][j][k]:
+                    cbt = '↓'
+                else:
+                    raise ValueError('something wrong in calculation')
+                    
+                s[i][j][k] = cs
+                bt[i-1][j-1][k-1] = cbt
 
 
-with open("dataset.txt","r") as f : 
-    data = f.readlines()
-    for i in range(len(data)):
-        data[i]=data[i].replace("\n","")
-    dnas=list()
-    for line in data : 
-        dnas.append(line)
-print(dnas)
+    ### part 2: distribute gaps in the sequences and calculate score
+    
+    i, j, k = len1 - 1, len2 - 1, len3 - 1
+    seq1_mod = seq2_mod = seq3_mod = ''   # modified versions of strings
+    sc = 0                                # score
+    
+    while i != -1 and j != -1 and k != -1:
+        ch = bt[i][j][k]
+        if ch == '↓':
+            seq1_mod += seq1[i]
+            seq2_mod += '-'
+            seq3_mod += '-'
+            i -= 1
+        elif ch == '→':
+            seq1_mod += '-'
+            seq2_mod += seq2[j]
+            seq3_mod += '-'
+            j -= 1
+        elif ch == '⨂':
+            seq1_mod += '-'
+            seq2_mod += '-'
+            seq3_mod += seq3[k]
+            k -= 1
+        elif ch == '↘':
+            seq1_mod += seq1[i]
+            seq2_mod += seq2[j]
+            seq3_mod += '-'
+            i -= 1
+            j -= 1  
+        elif ch == '⇓':
+            seq1_mod += seq1[i]
+            seq2_mod += '-'
+            seq3_mod += seq3[k]
+            i -= 1
+            k -= 1
+        elif ch == '⇒':
+            seq1_mod += '-'
+            seq2_mod += seq2[j]
+            seq3_mod += seq3[k]
+            j -= 1
+            k -= 1
+        elif ch == 'd':
+            seq1_mod += seq1[i]
+            seq2_mod += seq2[j]
+            seq3_mod += seq3[k]
+            if seq1[i] == seq2[j] == seq3[k]:
+                sc += 1
+            i -= 1
+            j -= 1
+            k -= 1
+        else:
+            raise ValueError(f'Unrecognize symbol in matrix: {ch}')
+
+            
+    # finalize lines after previous loop (simplest approach)
+    while i >= 0 or j >= 0 or k >= 0:
+        if (j==-1) and (k==-1):
+            seq1_mod += seq1[i]
+            seq2_mod += '-'
+            seq3_mod += '-'
+            i -= 1
+        elif (i==-1) and (k==-1):
+            seq1_mod += '-'
+            seq2_mod += seq2[j]
+            seq3_mod += '-'
+            j -= 1
+        elif (i==-1) and (j==-1):
+            seq1_mod += '-'
+            seq2_mod += '-'
+            seq3_mod += seq3[k]
+            k -= 1
+        elif k==-1:
+            seq1_mod += seq1[i]
+            seq2_mod += seq2[j]
+            seq3_mod += '-'
+            i -= 1
+            j -= 1  
+        elif j==-1:
+            seq1_mod += seq1[i]
+            seq2_mod += '-'
+            seq3_mod += seq3[k]
+            i -= 1
+            k -= 1
+        elif i==-1:
+            seq1_mod += '-'
+            seq2_mod += seq2[j]
+            seq3_mod += seq3[k]
+            j -= 1
+            k -= 1
+        else:
+            raise ValueError(f'Something wrong at the step of finalization of lines')
+
+    return seq1_mod[::-1], seq2_mod[::-1], seq3_mod[::-1], sc
+seq1 = 'CCAATACGAC'
+seq2 = 'GCCTTACGCT'
+seq3 = 'CCCTAGCGGC'
+
+seq1_mod, seq2_mod, seq3_mod, sc = MultipleLongestCommonSubsequence_simplest(seq1, seq2, seq3)
+print(sc)        #> 3
+print(seq1_mod)  #> AT-ATCCG-
+print(seq2_mod)  #> -T---CCGA
+print(seq3_mod)  #> ATGTACTG-
